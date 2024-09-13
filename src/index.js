0ea7cd969cfe1dcb546ce7e1df323cce2b8d5e4f
Fadziso Gabrielle <fadziso@novodigital.biz>
2024-09-13 17:27:39 +0200

    Added weather details

--- src/index.js
+++ src/index.js
@@ -3,9 +3,41 @@ function refreshWeather(response) {
     let temperature =response.data.temperature.current;
     temperatureElement.innerHTML = Math.round(temperature);
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
     // to ensure the response is for the city typed in regardless of typos etc
     cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 
 function citySearch (city) {
     // make api call & update interface
